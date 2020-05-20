b34e40eadb99f9780fe1dbe987e5bd99550d55bc
Alan <alanvega002@gmail.com>
2020-05-19 22:22:50 -0800

    Orbit.db + ipfs

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -7,7 +7,6 @@ import express, { Request, Response, NextFunction } from "express";
 import { BAD_REQUEST } from "http-status-codes";
 import "express-async-errors";
 
-import BaseRouter from "./routes";
 import logger from "@shared/Logger";
 
 // Init express
@@ -32,7 +31,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Add APIs
-app.use("/api", BaseRouter);
 
 // Print API errors
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -46,12 +44,16 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
  *                              Serve front-end content
  ***********************************************************************************/
 
-const viewsDir = path.join(__dirname, "views");
-app.set("views", viewsDir);
+const buildDir = path.join(__dirname, "build");
+
+app.set("build", buildDir);
+
 const staticDir = path.join(__dirname, "public");
+
 app.use(express.static(staticDir));
+
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile("index.html", { root: viewsDir });
+  res.sendFile("../../client/build/index.html", { root: buildDir });
 });
 
 // Export express instance
---
+++ server/src/web3/web3.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import web3 from "web3";
+import dotenv from "dotenv";
+dotenv.config("../env/.env");
+
+// {
+//     "constant": true,
+//     "inputs": [],
+//     "name": "totalSupply",
+//     "outputs": [
+//         {
+//             "name": "",
+//             "type": "uint256"
+//         }
+//     ],
+//     "payable": false,
+//     "type": "function"
+// },
